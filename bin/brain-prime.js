33d1003a59840ca156bea75e2ffc0db34607788c
tdd3vlp <tdd3vlp@gmail.com>
2024-03-01 22:08:06 +0300

    fix: minor update

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,5 +4,5 @@ import { showRules, getRandomNumber } from "../src/index.js";
 
 import isPrime from "../src/games/brain-prime.js";
 
-showRules("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+showRules('Answer "yes" if given number is prime. Otherwise answer "no".');
 isPrime(getRandomNumber(100));
